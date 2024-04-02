0ef3ee93f12afcfd5536b13c34090e32f6067ff7
Aaron Krapes <aaronkrapes@outlook.com>
2024-04-02 11:46:10 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(a) 
+{
+    var permutations = 0, sorted = false;
+    
+    while (!sorted)
+    {
+        if (a.length <= 1) { sorted = true; }
+        else { shuffle(a); }
+
+        permutations++;
+        sorted = checkSorted(a);
+    }
+
+    return permutations;
+}
+
+function checkSorted(a)
+{
+    for (var i = 0; i < a.length; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+// using Durstenfeld shuffle
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
